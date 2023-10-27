da9e0aeeaa97312b08a93cd00b559fce4aa1d577
Mohammedzxz712 <bdallhlym712@gmail.com>
2023-10-27 13:16:23 +0200

    watchlist done

--- lib/modules/watch_list/watch_screen.dart
+++ lib/modules/watch_list/watch_screen.dart
@@ -5,10 +5,67 @@ class WatchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("search"),
-      ),
+    return SafeArea(
+      child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (context, index) => Container(
+                height: 120,
+                child: Row(
+                  children: [
+                    Image(
+                      image: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNf8BJrOYOERBM0mYzBsj7rVR9fRXZwhNC8Q&usqp=CAU}'),
+                      // fit: BoxFit.cover,
+                      width: 120,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'lita Battle Angel',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                  height: 1.3, color: Colors.white),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              '2019',
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            // if (model.oldPrice != 0 &&
+                            //     isDiscount == true)
+                            Text(
+                              'Rosa Salazar, Christoph Waltz',
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: 10),
     );
   }
 }
